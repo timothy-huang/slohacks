c6f93de4823fe1426e980fbe6550abe4d30e0b93
Timothy <thuang87@berkeley.edu>
2019-02-02 21:26:27 -0800

    added some styling

---
+++ slohacksapp/slohacks-app/src/components/InputBox.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class InputBox extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            childInput: '',
+            expanded: false
+        }
+    }
+
+    
+    
+    handleChange = childInput => event => {
+        this.setState({
+          [childInput]: event.target.value,
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <h2>MacronutrientDetails</h2>
+                <div>{this.props.question}</div>
+            </div>
+        );
+    }
+}
+
+export default InputBox
